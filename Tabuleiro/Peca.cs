9a20751400ddb6f8946d457c0ec1f2bac41ae2f2
CarlosHp1996 <carloshpsantos1996@gmail.com>
2022-08-25 00:29:42 -0300

    Tratando erros com exceçao personalizada

--- Tabuleiro/Peca.cs
+++ Tabuleiro/Peca.cs
@@ -2,7 +2,7 @@
 {
     class Peca
     {
-        public Posicao posicao { get; set; }
+        public Posicao? posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
